f2248e6bc754608c6dfcc219da5f4dd211667542
ismael <ismaelct.es@gmail.com>
2021-08-19 21:23:34 +0200

    Colacao Nuevo y ejercicio de adivinar el numero

---
+++ AdivinarNumero.py
@@ -0,0 +1,10 @@
+fixnumb = 3
+
+print("Tienes que adivinar un numero del 1 al 10.")
+
+guessnumb = int(input("¿Cual crees que es el número?: "))
+
+if fixnumb == guessnumb:
+    print("Enhorabuena has acertado")
+
+print("Fin del juego.")
--- Colacao.py
+++ Colacao.py
@@ -3,28 +3,18 @@ print("Me voy a la cocina")
 print("Abro la nevera")
 
 milk = input("¿Hay leche? (S/N)")
+cocoa = input("¿Hay colacao? (S/N)")
 
-if milk == "S" or milk == "s":
-    cocoa = input("¿Hay colacao? (S/N)")
-    if cocoa == "S" or cocoa == "s":
-        print(
-            "Sacamos la leche de la nevera y la echamos en un vaso"
-        )
-        print(
-              "Echamos colacao en el vaso"
-        )
-        print(
-              "Mezclamos"
-        )
-    if cocoa == "n" or "N":
-        print("Hay que comprar colacao")
+if milk != "S" or cocoa != "S":
+    print("Voy a comprar al super...")
+    if milk != "S":
+        print("Compro leche")
+    if cocoa != "S":
+        print("Compro colacao")
 
-if milk == "n" or "N":
-    print("Hay que comprar leche")
-
-
-
-
-print(
-    "Con leche o sin leche hemos acabado"
-)
+if milk == "S":
+    print("Sacamos la leche de la nevera y la echamos en un vaso")
+if cocoa == "S" and milk == "S":
+    print("Echamos el colacao")
+print("Con leche o sin leche hemos acabado")
+print("Mezclamos")
