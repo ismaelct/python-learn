b4f2e3d882fe3dc05ec9f67f0d6a421e7806c0f4
ismael <ismaelct.es@gmail.com>
2021-09-07 21:20:01 +0200

    Pokemon 2.0

--- parte-2/#3B Combate Pokemon.py
+++ parte-2/#3B Combate Pokemon.py
@@ -1,64 +1,104 @@
 import random
 acabar = "S"
 while acabar == "S":
+
+    # Stats Pokemon A
     pokemon_a = "Pikachu"
     vida_pokemon_a = random.randint(75,100)
+    vida_actual_a = int(vida_pokemon_a)
     danno_pokemon_a = random.randint(5,15)
     ataque_rapido = int(random.uniform(0.5,1.5) * danno_pokemon_a)
     bola_voltio = int(2.5 * danno_pokemon_a)
     if vida_pokemon_a < 85:
         danno_pokemon_a += 5
 
+    # Stats Pokemon B
     pokemon_b = "Tortoise"
     vida_pokemon_b = random.randint(75,100)
+    vida_actual_b = int(vida_pokemon_b)
     danno_pokemon_b = random.randint(5,15)
     placaje = int(1.1 * danno_pokemon_b)
     hoja_afilada = 3 * danno_pokemon_b
     if vida_pokemon_b < 85:
         danno_pokemon_b += 5
 
+    # Definicion contador de rondas
     i = 0
+
+    # Mensaje principal
     print("Va a comenzar el combate Pokemón entre {} y {}\n".format(pokemon_a,pokemon_b))
 
-    while vida_pokemon_a > 0 and vida_pokemon_b > 0:
+    # Bucle de la pelea
+    while vida_actual_a > 0 and vida_actual_b > 0:
 
+        # Definicion de variables para representar la vida en %
+        porcentaje_vida_a = int(((100 * vida_actual_a) / vida_pokemon_a) / 10)
+        porcentaje_vida_b = int(((100 * vida_actual_b) / vida_pokemon_b) / 10)
+
+        # Condicion para acabar con el bucle en el turno del Pokemon A o B
+        if vida_actual_b <= 0 or vida_actual_a <= 0:
+            break
+
+        # Contador de rondas
         i += 1
+
+        # Mensaje con nombres de los pokemons y sus vidas
         print("Ronda {}.\n"
-              "Pikachu [{}]PS || Tortoise [{}]PS\n".format(i,vida_pokemon_a * "█",vida_pokemon_b * "█"))
+              "Pikachu  [{}] {} PS\n"
+              "Tortoise [{}] {} PS\n".format(i,porcentaje_vida_a * "█" + ((10 - porcentaje_vida_a) * " "),vida_actual_a
+                                         ,porcentaje_vida_b * "█" + ((10 - porcentaje_vida_b) * " "),vida_actual_b))
 
         # Turno Pikachu
-        print("Turno de {}\n Vida de {} [{}]".format(pokemon_a,pokemon_a,vida_pokemon_a * "█"))
+        print("Turno de {} [{}]".format(pokemon_a,porcentaje_vida_a * "█"\
+                                                     + ((10 - porcentaje_vida_a) * " ")))
 
+        # Decision de ataque poquemon 1
         ataque_cpu = random.randint(1,2)
 
+        # Condicion de elcción de ataque y consecuencias
         if ataque_cpu == 1:
-            print("{} ha usado [ataque rapido] y ha hecho {} de daño.\n".format(pokemon_a,ataque_rapido))
-            vida_pokemon_b = vida_pokemon_b - ataque_rapido
+            print("{} ha usado [ataque rapido] y ha hecho {} de daño.".format(pokemon_a,ataque_rapido))
+            vida_actual_b = vida_actual_b - ataque_rapido
         else:
             print("{} ha usado [bola voltio] y ha hecho {} de daño.".format(pokemon_a, bola_voltio))
-            vida_pokemon_b -= bola_voltio
+            vida_actual_b -= bola_voltio
             autodanno_a = random.randint(2,5)
-            vida_pokemon_a -= autodanno_a
-            print("{} se ha hecho {} punto(s) de daño.\n".format(pokemon_a,autodanno_a))
-        if vida_pokemon_a <= 0 or vida_pokemon_b <= 0:
+            vida_actual_a -= autodanno_a
+            print("{} se ha hecho {} punto(s) de daño.".format(pokemon_a,autodanno_a))
+
+        input("Presiona enter para continuar...")
+
+        # Condicion para acabar con el bucle en el turno del Pokemon B o A
+        if vida_actual_a <= 0 or vida_actual_b <= 0:
             break
+
         # Turno Tortoise
-        print("Turno de {}\n Vida de {} [{}]".format(pokemon_b,pokemon_b,vida_pokemon_b * "█"))
-        ataque_jugador = int(input("¿Cual Sera tu ataque?:\n"
-                                   "[1] Placaje \\ [2] Hoja afilada\n"))
+        print("\nTurno de {} [{}]".format(pokemon_b,porcentaje_vida_b * "█" + ((10 - porcentaje_vida_b) * " ")))
 
-        if ataque_jugador == 1:
-            print("{} ha usado [placaje] y ha hecho {} de daño.\n".format(pokemon_b, placaje))
-            vida_pokemon_a -= placaje
-        else:
+        ataque_jugador = None
+
+        while ataque_jugador != "P" and ataque_jugador != "H":
+
+            ataque_jugador = input("¿Cual Sera tu ataque?:\n"
+                                   "[P] Placaje \\ [H] Hoja afilada\n")
+
+        # Condicion de elcción de ataque y consecuencias
+        if ataque_jugador == "P":
+            print("{} ha usado [placaje] y ha hecho {} de daño.".format(pokemon_b, placaje))
+            vida_actual_a -= placaje
+        elif ataque_jugador == "H":
             print("{} ha usado [hoja afilada] y ha hecho {} de daño.".format(pokemon_b, hoja_afilada))
-            vida_pokemon_a -= hoja_afilada
+            vida_actual_a -= hoja_afilada
             autodanno_b = random.randint(1,7)
-            vida_pokemon_b -= autodanno_b
-            print("{} se ha hecho {} punto(s) de daño.\n".format(pokemon_b, autodanno_b))
+            vida_actual_b -= autodanno_b
+            print("{} se ha hecho {} punto(s) de daño.".format(pokemon_b, autodanno_b))
 
-    if vida_pokemon_a > vida_pokemon_b:
-        print("Ha ganado {} con {}PS".format(pokemon_a,vida_pokemon_a * "█"))
+        input("Presiona enter para continuar...\n")
+
+    if vida_actual_a > vida_actual_b:
+        print("Ha ganado {} con [{}]PS".format(pokemon_a,porcentaje_vida_a * "█" + ((10 - porcentaje_vida_a) * " ")))
     else:
-        print("Ha ganado {} con [{}]PS".format(pokemon_b,vida_pokemon_b * "█"))
-    acabar = str(input("Quieres volver a jugar: [S]i | [N]o\n"))
+        print("Ha ganado {} con [{}]PS".format(pokemon_b,porcentaje_vida_b * "█" + ((10 - porcentaje_vida_b) * " ")))
+        acabar = None
+        while acabar != "S" or acabar != "N":
+            acabar = str(input("Quieres volver a jugar: [S]i | [N]o\n"))
