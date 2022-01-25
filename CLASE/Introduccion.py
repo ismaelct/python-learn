2ff500b239a8169f836181425962bd8947bb0bcb
Ismael <angar@pop-os.localdomain>
2022-01-25 12:44:01 +0100

    mar 25 ene 2022 12:44:01 CET

---
+++ CLASE/Introduccion.py
@@ -0,0 +1,62 @@
+# variables
+nombre = 'ismael'
+pi = '3.1415'
+
+# cadenas
+solo = 'ismael'
+multi = """
+ismael
+castellanos
+talavera
+"""
+
+# numeros
+entero = 192
+decimal = 1.92
+
+# Boelanos
+verdadero = True
+falso = False
+
+# Listas tuplas y diccionarios
+# tabla = (hola,adios,quetal)
+
+# Operadores
+# + suma
+# - resta
+# * multiplicacion
+# / division
+# ** potencia
+# // cociente de la division entera
+# % resto de la division entera
+a = 1
+b = 2
+print("a b")
+print(a,b)
+a = a + b
+print(a,b)
+a += b
+print(a,b)
+# ==
+# !=
+# >
+# <
+# >=
+# <=
+# and
+    # true and true = true
+    # true and false = false
+# or
+    # true or true = true
+    # true or false = true
+    # false or false = false
+# xor
+    # true xor false = true
+    # true xor false = false
+    # false xor false = false
+
+# Imprimir pantalla
+print("Mensaje","alerta",a,sep="_",end=" :termiando")
+
+# pedir informacion
+variable = input("Dime algo: ")  # raw_input()
